3dde6b9bcc548ef39b99a8ec6011a8872e6ad8d7
tommy <tonlien89@yahoo.com.tw>
2023-07-25 21:01:57 +0800

    0725

--- routes/cart.js
+++ routes/cart.js
@@ -112,23 +112,29 @@ const cartsql = `SELECT book_info.pic,book_info.book_name,book_info.ISBN,book_in
     output = {...output, cart}; //取代輸出預設值 為正確數值
       return res.json(output);
 })
+
 router.post('/cart/plus',async(req,res)=>{
     const ISBN = req.body.ISBN;
     const plussql = `UPDATE cart SET count = count + 1, updateAt = ? WHERE ISBN = ? AND member_id = 1`;
     const [updateResult] = await db.query(plussql, [currentDateTime,ISBN]);
-    console.log(updateResult);
     const updatedCount = updateResult.affectedRows === 1 ? updateResult.changedRows : 0;
     return res.json({ updatedCount });
 })
 
 router.post('/cart/cut',async(req,res)=>{
   const ISBN = req.body.ISBN;
-  const cutsql = `UPDATE cart SET count = count - 1, updateAt = ? WHERE ISBN = ? AND member_id = 1`;
+  console.log(122333);
+  console.log(ISBN);
+  const cutsql = `UPDATE cart SET count = count-1, updateAt = ? WHERE ISBN = ? AND member_id = 1`;
   const [updateResult] = await db.query(cutsql, [currentDateTime,ISBN]);
+  console.log(updateResult);
+
   if (updateResult.affectedRows === 1 && updateResult.changedRows === 1) {
     const checkSql = `SELECT count FROM cart WHERE ISBN = ? AND member_id = 1`;
     const [checkResult] = await db.query(checkSql, [ISBN]);
-    if ( checkResult[0].count < 1) {
+    const updatedCount = checkResult[0].count;
+
+    if ( updatedCount < 1) {
       const deleteSql = `DELETE FROM cart WHERE ISBN = ? AND member_id = 1`;
       const [deleteResult] = await db.query(deleteSql, [ISBN]);
       return res.json(deleteResult)
@@ -140,7 +146,7 @@ router.post('/cart/cut',async(req,res)=>{
 router.post('/cart/delete',async(req,res)=>{
   const ISBN = req.body.ISBN;
   const deletesql = `DELETE FROM cart WHERE ISBN = ? AND member_id = 1`;
-  const [deleteResult] = await db.query(deletesql, [ISBN]);
+  const [deleteResult] = await db.query(deletesql, [ISBN]);                        
   return res.json(deleteResult)
 })
 
